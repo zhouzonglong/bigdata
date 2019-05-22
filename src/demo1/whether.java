40dce0fb6c405023fe82ea9ecd68e1627983204c
tails <852037154@qq.com>
2019-05-22 21:47:12 +0800

    java code :insert into Hbase
    2019.5.22

---
+++ src/demo1/whether.java
@@ -0,0 +1,40 @@
+package demo1;
+
+public class whether {
+	private Integer id;
+	private String addr;
+	private String type;
+	private String date;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+}
